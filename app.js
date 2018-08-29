3b8d3ffdc1082d777d36b77cf0845130219b6d35
sajith sajeev <sajithsajeevr@outlook.com>
2018-08-30 02:19:39 +0530

    dynamic functionality

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 
-//Given Array
+document.getElementById('bttn').addEventListener('click',removeDuplicates);
+
+const ul=document.getElementById('array');
 let array=[1,1,2,3,4,5,6,6,7];
-
-const noDuplicates=array.filter(function(item,pos,return1){
-return return1.indexOf(item)==pos;
-});
+array.forEach(element=>ul.innerHTML+=`<li>${element}</li>`);
 
 
-console.log(noDuplicates);
+function removeDuplicates(e){
+    const noduplicatearray=document.getElementById('noduplicate');
+    let noDuplicates=array.filter((item,pos,return1)=>{return return1.indexOf(item)==pos;});
+    noDuplicates.forEach(element=>noduplicatearray.innerHTML+=`<li>${element}</li>`);
+    document.getElementById('bttn').style.display='none';
+    document.getElementById('noduplicatearrayhead').innerHTML="<em>Array with no Duplicate Elements<em>";
+    e.preventDefault();
+}
